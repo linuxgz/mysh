4d6f8d05821a858faf2666934fb0b9b86578ba0b
linuxgz <linuxgz@163.com>
2021-08-14 01:26:18 +0800

    quick save info

--- git_aliases.sh
+++ git_aliases.sh
@@ -6,4 +6,4 @@ alias Mgt='git commit'
 alias Mgta='git commit -a'
 alias Mgpl='git pull git@github.com:linuxgz/mysh master'
 alias Mgps='git push git@github.com:linuxgz/mysh master'
-
+alias MG='Mgta -m "quick save info" && Mgps'
