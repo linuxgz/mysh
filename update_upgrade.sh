9e38549305c7fbc4466ee92b4a5200394023ad0b
linuxgz <linuxgz@163.com>
2021-08-14 09:44:42 +0800

    quick save info

--- update_upgrade.sh
+++ update_upgrade.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo " it may take long time to finish."
-
-sudo apt-get update && sudo apt-get upgrade -y
+sudo apt update && sudo apt upgrade -y
+# sudo apt-get update && sudo apt-get upgrade -y
