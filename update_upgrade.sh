7e95332c7adfe517d094a6d7612060d97a1aaf87
linuxgz <linuxgz@163.com>
2021-08-15 10:22:03 +0800

    quick save info

--- update_upgrade.sh
+++ update_upgrade.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 echo " it may take long time to finish."
-sudo apt update && sudo apt upgrade -y
+sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade
+
 # sudo apt-get update && sudo apt-get upgrade -y
+
+# apt-get upgrade #更新已经安装的软件
+# aptitude safe-upgrade
+
+# apt-get dist-upgrade # 升级系统
+# aptitude full-upgrade # same
