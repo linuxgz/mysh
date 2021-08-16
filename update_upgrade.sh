ad2afa9062ab1fca6400a75c865420561530c94e
linuxgz <linuxgz@163.com>
2021-08-17 01:09:05 +0800

    save them

--- update_upgrade.sh
+++ update_upgrade.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo " it may take long time to finish."
-sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade
-
+sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade || sudo  apt --fix-broken install && echo "well done"
 # sudo apt-get update && sudo apt-get upgrade -y
 
 # apt-get upgrade #更新已经安装的软件
